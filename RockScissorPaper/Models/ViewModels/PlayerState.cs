7c25278b15c17a77183bde522f797ba212736d80
PjMitchell <peter@hilltop-digital.co.uk>
2013-03-30 14:42:04 +0000

    Added Player State

---
+++ RockScissorPaper/Models/ViewModels/PlayerState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockScissorPaper.Models
+{
+    public class PlayerState
+    {
+        public int PlayerId { get; set; }
+        public RoshamboSelection CurrentSelection { get; set; }
+        public bool IsViewer { get; set; }
+        public int CurrentScore { get; set; }
+        public string PlayerMessage { get; set; }
+    }
+}
