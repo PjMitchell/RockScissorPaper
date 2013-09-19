7b9a321e3813ccb2fdf4be61ee9713071f3b1741
PjMitchell <peter@hilltop-digital.co.uk>
2013-09-19 11:09:35 +0100

    Added Grouped by date statistic stored proceedure

--- SQLScripts/MySQLScripts/StoredProceedures/Statistics/Gameround_GetGroupedByDate.sql
+++ SQLScripts/MySQLScripts/StoredProceedures/Statistics/Gameround_GetGroupedByDate.sql
@@ -1,24 +1,20 @@
-DROP PROCEDURE IF EXISTS `GameRound_GetStatistic`;
+DROP PROCEDURE IF EXISTS `Gameround_GetGroupedByDate`;
 DELIMITER $$
 
 
-CREATE PROCEDURE `GameRound_GetStatistic` ()
+CREATE PROCEDURE `Gameround_GetGroupedByDate` ()
 
 BEGIN
-SELECT RoundNumber,
+
+SELECT Date(game.StartDate) as 'Date',
  SUM(CASE WHEN SelectionId = 1 THEN 1 ELSE 0 END) as Rock,
  SUM(CASE WHEN SelectionId = 2 THEN 1 ELSE 0 END) as Scissor,
  SUM(CASE WHEN SelectionId = 3 THEN 1 ELSE 0 END) as Paper
  FROM gameroundresult as grr
  Inner Join gameround as gr on grr.GameRoundId = gr.GameRoundId
  Inner Join player as p on p.PlayerId = grr.PlayerId
+ Inner Join roshambogame as game on grr.RoshamboGameId = game.RoshamboGameId 
  Where p.IsBot = 0
- Group by RoundNumber
+ Group by Date(game.StartDate)
+ Order by Date(game.StartDate)
  ;
-
-
-END$$
-
-
-
-DELIMITER ;
