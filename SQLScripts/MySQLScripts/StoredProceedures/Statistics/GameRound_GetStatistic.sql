1a8c3c4e7ecc5b56e8322ecdf4aa953cbe95d049
PjMitchell <peter@hilltop-digital.co.uk>
2013-09-20 12:18:18 +0100

    Modified Statistics Proceedure to use shared View

--- SQLScripts/MySQLScripts/StoredProceedures/Statistics/GameRound_GetStatistic.sql
+++ SQLScripts/MySQLScripts/StoredProceedures/Statistics/GameRound_GetStatistic.sql
@@ -9,12 +9,8 @@ SELECT RoundNumber,
  SUM(CASE WHEN SelectionId = 1 THEN 1 ELSE 0 END) as Rock,
  SUM(CASE WHEN SelectionId = 2 THEN 1 ELSE 0 END) as Scissor,
  SUM(CASE WHEN SelectionId = 3 THEN 1 ELSE 0 END) as Paper
- FROM gameroundresult as grr
- Inner Join gameround as gr on grr.GameRoundId = gr.GameRoundId
- Inner Join player as p on p.PlayerId = grr.PlayerId
- Where p.IsBot = 0
- Group by RoundNumber
- ;
+ FROM roundselectionstatistics
+ Group by RoundNumber;
 
 
 END$$
--- SQLScripts/MySQLScripts/StoredProceedures/Statistics/Gameround_GetGroupedByDate.sql
+++ SQLScripts/MySQLScripts/StoredProceedures/Statistics/Gameround_GetGroupedByDate.sql
@@ -6,18 +6,13 @@ CREATE PROCEDURE `Gameround_GetGroupedByDate` ()
 
 BEGIN 
 
-SELECT Date(game.StartDate) as 'Date',
+SELECT Date(StartDate) as 'Date',
  SUM(CASE WHEN SelectionId = 1 THEN 1 ELSE 0 END) as Rock,
  SUM(CASE WHEN SelectionId = 2 THEN 1 ELSE 0 END) as Scissor,
  SUM(CASE WHEN SelectionId = 3 THEN 1 ELSE 0 END) as Paper
- FROM gameroundresult as grr
- Inner Join gameround as gr on grr.GameRoundId = gr.GameRoundId
- Inner Join player as p on p.PlayerId = grr.PlayerId
- Inner Join roshambogame as game on grr.RoshamboGameId = game.RoshamboGameId 
- Where p.IsBot = 0
- Group by Date(game.StartDate)
- Order by Date(game.StartDate)
- ;
+ FROM roundselectionstatistics
+ Group by Date(StartDate)
+ Order by Date(StartDate);
 END$$
 
 
