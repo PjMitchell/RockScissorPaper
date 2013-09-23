14b7797989f2044683e363632efa4060f83b7430
PjMitchell <peter@hilltop-digital.co.uk>
2013-09-23 15:10:37 +0100

    Split View Script into separate Script file

---
+++ SQLScripts/MySQLScripts/Views/RoundSelectionStatistics.sql
@@ -0,0 +1,9 @@
+CREATE VIEW RoundSelectionStatistics AS
+
+SELECT selectionId, RoundNumber, StartDate, ButtonOrder
+FROM gameroundresult as grr
+Inner Join gameround as gr on grr.GameRoundId = gr.GameRoundId
+Inner Join player as p on p.PlayerId = grr.PlayerId
+Inner Join roshambogame as game on grr.RoshamboGameId = game.RoshamboGameId 
+Where p.IsBot = 0
+;
