d47456c2e12fc8cfde08cddcf0dd30770394a50c
PjMitchell <peter@hilltop-digital.co.uk>
2013-09-24 13:07:56 +0100

    Added GetSelectionVsTime_TableDimensionAreCorrect Test

---
+++ RockScissorPaper.DAL.Test/Tests/StatisticsSQLRepositoryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RockScissorPaper.DAL.Test
+{
+    [TestClass]
+    public class StatisticsSQLRepositoryTests
+    {
+        [TestMethod]
+        public void CanGetTotalGamesPlayed()
+        {
+            FakeDBService service = FakeDBService.GetInstance();
+            StatisticsSQLRepository repository = new StatisticsSQLRepository(service.GetDatabaseConnector());
+            int expected = 1;
+
+            int result = repository.GetGamesPlayed();
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GetSelectionVsTime_TableDimensionsAreCorrect()
+        {
+            FakeDBService service = FakeDBService.GetInstance();
+            StatisticsSQLRepository repository = new StatisticsSQLRepository(service.GetDatabaseConnector());
+            int expectedRowCount = 1;
+            int expectedColumnCount = 4;
+
+
+            DataTable result = repository.GetSelectionVsTime();
+            int rowCount = result.Rows.Count;
+            int columnCount = result.Columns.Count;
+
+            Assert.AreEqual(expectedRowCount, rowCount);
+            Assert.AreEqual(expectedColumnCount, columnCount);
+        }
+    }
+}
---
+++ SQLScripts/MSSQLScripts/UNDO-DatabaseStartUpScript.sql
@@ -0,0 +1,10 @@
+DROP VIEW RoundSelectionStatistics;
+DROP TABLE GameRoundResult;
+DROP TABLE GameRound;
+DROP TABLE RoshamboSelection;
+DROP TABLE GamePlayer;
+DROP TABLE GameResult;
+DROP TABLE Player;
+DROP TABLE RoshamboGame;
+DROP TABLE GameRuleSet;
+DROP TABLE GameStatus;
